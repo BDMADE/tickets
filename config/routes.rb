c372e45ce509eb32500ce329c04b9415e10c8fe5
hmtanbir <tanbir2025@gmail.com>
2017-07-17 00:16:25 +0600

    add config

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  resources :users
+  resources :tickets do
+    resources :ticket_replies
+  end
+  resources :user_types
+  resources :sessions, only: [:new, :create, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'password_changes', to: 'password_changes#edit', as: 'password_changes'
+  post 'password_changes', to: 'password_changes#update'
+  root 'dashboard#index'
+  get 'dashboard', to: 'dashboard#index'
+  get 'welcome', to: 'dashboard#welcome'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'registrations', to: 'users#registration', as: 'registrations'
+  post 'registrations', to: 'users#create'
+
+  get 'show_ticket/:id', to: 'tickets#show_ticket'
+end
